49bb50a828c4bc4672d7565ddc742418d996cb2f
jatin.g <jatin.g@media.net>
2016-07-20 12:48:11 -0700

    Hello

---
+++ scripts/jmain.js
@@ -0,0 +1,64 @@
+var notes=[],
+	$addNote=$('.header'),
+	addNoteForm=$addNote.find('.header-form'),
+	$notes=$('.note-container'),
+	noteTitle=addNoteForm.find('input[name="note_title"]'),
+	noteContent=addNoteForm.find('textarea[name="note_content"]')
+
+var cnt=0;
+function appendSingleNote(data){
+	var content=data.content,title=data.title,count=data.id;
+	 var html = '<div class="note" id="' + count + '">' +
+                    '<p class="note-title">' +
+                    '<button onclick="removeNote(' + count + ')" color:black; height: 20px; width: 20px"> </button>' + 
+                    title + 
+                    '</p>' +
+                    '<p class="note-content">' +
+                        content + 
+                    '</p>' + 
+                '</div>';
+	$notes.append(html);
+	cnt++;
+}
+
+function storeNote(data){
+	notes.push(data);
+	window.localStorage.setItem('notes',JSON.stringify(notes));
+	appendSingleNote(data);
+}
+
+
+function init(){
+	if(!!window.localStorage.getItem('notes')){
+
+		notes=JSON.parse(window.localStorage.getItem('notes'));
+	}
+	else{
+
+		alert('null');
+		notes=[];
+	}
+}
+
+addNoteForm.on('submit',function(e){
+	e.preventDefault();
+	// alert("click");
+	var data= {title:noteTitle.val(),
+		content:noteContent.val(),id:cnt};
+	console.log(data);
+	storeNote(data);
+})
+
+
+function removeNote(id) {
+	console.log(id);
+
+    notes = notes.filter(function(e) {
+        return e.id !== id;
+    });
+    window.localStorage.setItem('notes', JSON.stringify(notes));
+    
+    var parent = document.getElementById('notes_all');
+    var child = document.getElementById(id);
+    parent.removeChild(child);
+}
